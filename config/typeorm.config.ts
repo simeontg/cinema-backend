86dd278c5ec3d97881e4788b648b7f928f0faddf
Simeon Georgiev <sgeorgiev783@gmail.com>
2024-06-28 10:19:36 +0300

    revert env variables for migrations

--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -8,8 +8,8 @@ const migrationsTableName = process.env.MIGRATIONS_TYPE === 'seed' ? 'seeds' : '
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5131,
+  host: process.env.MIGRATIONS_HOST,
+  port: +process.env.MIGRATIONS_PORT,
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
