fb562398e772d481e8cc386c4adb1f002acebf8a
Simeon Georgiev <sgeorgiev783@gmail.com>
2024-07-02 18:24:40 +0300

    address comments

--- migrations/1719483706248-CreateProfileTable.ts
+++ migrations/1719483706248-CreateProfileTable.ts
@@ -7,8 +7,6 @@ export class CreateProfileTable1719483706248 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "profiles" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "phoneNumber" character varying NOT NULL, CONSTRAINT "PK_8e520eb4da7dc01d0e190447c8e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "users" ADD "profileId" uuid`);
         await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_b1bda35cdb9a2c1b777f5541d87" UNIQUE ("profileId")`);
-        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_b1bda35cdb9a2c1b777f5541d87" FOREIGN KEY ("profileId") REFERENCES "profiles"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_b1bda35cdb9a2c1b777f5541d87"`);
         await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_b1bda35cdb9a2c1b777f5541d87" FOREIGN KEY ("profileId") REFERENCES "profiles"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
@@ -17,8 +15,6 @@ export class CreateProfileTable1719483706248 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_b1bda35cdb9a2c1b777f5541d87"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "profileId"`);
         await queryRunner.query(`DROP TABLE "profiles"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_b1bda35cdb9a2c1b777f5541d87"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_b1bda35cdb9a2c1b777f5541d87" FOREIGN KEY ("profileId") REFERENCES "profiles"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
-
 }
+
--- migrations/1719492374845-CreateRoleTable.ts
+++ migrations/1719492374845-CreateRoleTable.ts
@@ -4,7 +4,8 @@ export class CreateRoleTable1719492374845 implements MigrationInterface {
     name = 'CreateRoleTable1719492374845'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "roles" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "public"."roles_name_enum" AS ENUM('USER', 'ADMIN')`);
+        await queryRunner.query(`CREATE TABLE "roles" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "name" "public"."roles_name_enum" NOT NULL, CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "users" ADD "roleId" uuid`);
         await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_368e146b785b574f42ae9e53d5e" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -13,6 +14,6 @@ export class CreateRoleTable1719492374845 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_368e146b785b574f42ae9e53d5e"`);
         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "roleId"`);
         await queryRunner.query(`DROP TABLE "roles"`);
+        await queryRunner.query(`DROP TYPE "public"."roles_name_enum"`);
     }
-
 }
